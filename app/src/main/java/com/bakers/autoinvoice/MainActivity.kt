cee41f2e2952025344d27599498e1edc9a07f717
dizzi7891 <bakersauto2020@gmail.com>
2025-11-09 14:24:28 -0500

    Update MainActivity.kt

--- app/src/main/java/com/bakers/autoinvoice/MainActivity.kt
+++ app/src/main/java/com/bakers/autoinvoice/MainActivity.kt
@@ -4,30 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Baker's Auto")
+                Surface {
+                    Text("AutoInvoice App")
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello, $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaterialTheme {
-        Greeting("Preview")
     }
 }
