92e310cd02e7f2ad556a5c768d9ae1f0f8995ba1
dizzi7891 <bakersauto2020@gmail.com>
2025-11-23 15:09:36 -0500

    Create Color.kt

---
+++ app/src/main/java/com/bakers/autoinvoice/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.bakers.autoinvoice.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Maroon = Color(0xFF800000)
+val MaroonLight = Color(0xFFB22222)
+val AccentGold = Color(0xFFFFC107)
